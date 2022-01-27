10b647ca75b2c533f1e0cfd431411d864b3beaa1
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-01-26 22:47:16 -0300

    fix: :bug: arrumando regex da removeAccentSpecialCharacters, pra que permaneça o espaço em branco

--- src/database/service/candidates.js
+++ src/database/service/candidates.js
@@ -1,5 +1,6 @@
 const { Candidates } = require('../models');
 const { scrapingDataCandidates, scrapingDataNameAndScore } = require('../../utils/dataScraping');
+const { removeAccentsAndSpecialCharacters } = require('../../utils/formatDatas')
 const { cpf } = require('cpf-cnpj-validator');
 
 const getValidCPFs = async () => {
@@ -24,8 +25,9 @@ const saveApprovedCandidates = async () => {
     const saveCandidates = candidatesCPFs.map(async(cpf)  => {
 
       const { name, score } = await scrapingDataNameAndScore(cpf);
+      const nameFormatted = removeAccentsAndSpecialCharacters(name);
 
-      const candidate = await Candidates.upsert({ cpf, name, score });
+      const candidate = await Candidates.upsert({ cpf, name: nameFormatted, score });
   
       return candidate[0];
     });
---
+++ src/utils/formatDatas.js
@@ -0,0 +1,10 @@
+const removeAccentsAndSpecialCharacters = (string) => {
+    /*referência:  https://pt.stackoverflow.com/questions/124754/retirar-caracteres-especial-e-acentos-em-javascript */
+    const formattedName = string.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/[^a-z0-9/\s]/gi,'').toLocaleUpperCase();
+    
+    return formattedName;
+}
+
+module.exports = {
+  removeAccentsAndSpecialCharacters
+}
